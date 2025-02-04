9a316f9c9694d2e3ed80827b026ca64366aa4146
Gaurav Phuyal <phuyalgaurav90@gmail.com>
2025-02-03 20:15:14 -0700

    Updated Docker & Added PDF Generation

--- resume/views.py
+++ resume/views.py
@@ -4,9 +4,23 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.conf import settings
+from .utils import generate_data, generate_pdf_template_1
+import json
+
 
 def index(request):
-    return render(request, 'resume/getstarted.html')
+    return render(request, "resume/getstarted.html")
+
 
 def home(request):
-    return render(request, 'resume/home.html')
+    return render(request, "resume/home.html")
+
+
+# Testing purposes ko lagi matrai ho hai yo error ayo bhane comment garde yo function
+def generate_resume(request):
+    data = generate_data(
+        "Name: Gaurav Phuyal, Email:phuyalgaurav90@gmail.com, Experienced python developer with over 30 years of experience, Skills: Python, Django, Flask, FastAPI, JavaScript, React, Angular, Vue, HTML, CSS, SQL, NoSQL, Docker, Kubernetes, AWS, Azure, GCP, Git, CI/CD, Agile, Scrum, Kanban, TDD, BDD, DDD, Microservices, REST, GraphQL, etc..."
+    )
+
+    generate_pdf_template_1(json.loads(data))
+    return render(request, "resume/home.html")
